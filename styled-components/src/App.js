5089808d7e9a3634f888b823f0810754a2f3c74a
seungdang123 <seungdang123@gmail.com>
2022-01-14 13:43:51 +0900

    Import styled-components

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+// Styled-components: Useful way applying CSS
+// Use "backtic" for setting CSS properties
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // # Inline CSS: Easy but dirty code...
+    // <div style={{ display: "flex" }}>
+    //   <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+    //   <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+    // </div>
+
+    // # Styled-components: Easier to read than inline CSS
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
   );
 }
 
