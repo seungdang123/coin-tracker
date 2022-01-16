838983b19c56b086aec0698dea697dcc88384e87
seungdang123 <seungdang123@gmail.com>
2022-01-16 20:52:04 +0900

    Props and extend

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -6,20 +6,29 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+// const BoxOne = styled.div`
+//   background-color: teal;
+//   width: 100px;
+//   height: 100px;
+// `;
+
+// const BoxTwo = styled.div`
+//   background-color: tomato;
+//   width: 100px;
+//   height: 100px;
+// `;
+
+// Simplify above code
+// Use props
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
+// Extend components
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 function App() {
@@ -32,10 +41,8 @@ function App() {
 
     // # Styled-components: Easier to read than inline CSS
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
