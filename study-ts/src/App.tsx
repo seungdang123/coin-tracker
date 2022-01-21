efdd71c73720a92096b1121a27a1144d818e3688
seungdang123 <seungdang123@gmail.com>
2022-01-22 04:10:05 +0900

    Forms & Theme

--- study-ts/src/App.tsx
+++ study-ts/src/App.tsx
@@ -1,15 +1,48 @@
-// TypeScript definition : Make a explanation for TS
+// 1. TypeScript definition : Make a explanation for TS
 // Solve a problem by doing npm install -i --save-dev @types/styled-components for some error in related in TS
 // refer to "DefinitelyTyped"
 
-import Circle from "./Circle";
+// 2. Typing for Event
+// const [value, setValue] = useState("");
+// const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//   const {
+//     currentTarget: { value },
+//   } = event;
+//   setValue(value);
+// };
+
+// const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//   event.preventDefault();
+//   console.log("Hello", value);
+// };
+
+// <form onSubmit={onSubmit}>
+//   <input
+//     value={value}
+//     onChange={onChange}
+//     type="text"
+//     placeholder="username"
+//   />
+//   <button>Log in</button>
+// </form>
+
+// 3. Theme in TS
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle borderColor="blue" bgColor={"teal"} />
-      <Circle text="I'm hungry" bgColor={"tomato"} />
-    </div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
   );
 }
 
