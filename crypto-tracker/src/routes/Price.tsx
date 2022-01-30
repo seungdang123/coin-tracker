85d887b64c45461e848bd35390f8bcbe9dc8df07
seungdang123 <seungdang123@gmail.com>
2022-01-31 03:38:06 +0900

    Update price

--- crypto-tracker/src/routes/Price.tsx
+++ crypto-tracker/src/routes/Price.tsx
@@ -1,19 +1,30 @@
 import { useOutletContext } from "react-router-dom";
 import { Overview, OverviewItem } from "./Coin";
+import styled from "styled-components";
 
 interface PriceProps {
   tickersData: {
     quotes: {
       USD: {
         price: number;
-        percent_change_12h: number;
+        percent_change_24h: number;
       };
     };
   };
 }
 
+const Up = styled.span`
+  color: red;
+`;
+
+const Down = styled.span`
+  color: blue;
+`;
+
 function Price() {
   const { tickersData } = useOutletContext<PriceProps>();
+  const priceflow = tickersData.quotes.USD.percent_change_24h;
+
   return (
     <Overview>
       <OverviewItem>
@@ -21,8 +32,8 @@ function Price() {
         <span>{`$ ${tickersData.quotes.USD.price}`}</span>
       </OverviewItem>
       <OverviewItem>
-        <span>PERCENT CHANGE(12H)</span>
-        <span>{`${tickersData.quotes.USD.percent_change_12h}%`}</span>
+        <span>COMPARE TO YESTERDAY</span>
+        {priceflow < 0 ? <Down>{priceflow}%</Down> : <Up>{priceflow}%</Up>}
       </OverviewItem>
     </Overview>
   );
